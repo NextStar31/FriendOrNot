02119b281df9c1e8ab40fc54ccf1cf278ebe6558
AGIL <clement.agil@gmail.com>
2019-06-29 19:37:51 +0200

    files organisation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
 
 export default function App() {
+
+  _onStart = () => {
+   console.log('Press');
+  };
+
   return (
     <View style={styles.container}>
       <Text>Test your friendship ! </Text>
+
+      <Button
+              onPress={this._onStart}
+              title="Begin"
+              color="#e88f00"
+            />
     </View>
   );
 }
